65ed3f4fe9b062724a82bf032e100eae69bb38d6
Sergey Trubnikov <sergeytrubnikov1@gmail.com>
2019-05-21 16:40:53 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import settings
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    filename="bot.log"
+)
+
+
+
+def greet_user(bot, update):
+    text = "Called /start"
+    print(text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = '''Hello {}! Your typed "{}"'''.format(update.message.chat.first_name, update.message.text)
+    logging.info("User %s, Chat ID: %s, Message: %s", update.message.chat.username, update.message.chat.id, update.message.text)
+    update.message.reply_text(user_text)
+
+def main():
+    """The main fucntion for bot"""
+    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
+
+    logging.info("Bot is starting")
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    mybot.start_polling()
+    mybot.idle()
+
+main()
+
+
